ba46363dd0493feedb7d1762492d83fa911a841d
Uzama <uzamazaid@outlook.com>
2018-04-03 09:11:53 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,34 @@
+# python version 3.x
+# The first step of starting python is display a sentence or word on window
+# "print" key word prints 
+
+
+print ("Hello World") # basic syntax for print
+
+# result will be : Hello World
+# The result will be what is in between two "Quatation" marks.
+
+
+
+#KEY POINTS TO NOTE DOWN
+    # python is case sensitive ("print" is not equal to "Print")
+    # "#" symbol uses for writing comments in your codes.
+    # it will not affect you code
+    # you can use single quatation marks rather than using double qutation
+    
+
+
+#Excercises
+
+    # 1. display your name
+    # 2. display "Hello World" 5 times
+    # 3. display complete your biography
+
+
+# Stay tuned with next Tutorials.......
+
+    
+
+    
+
+
